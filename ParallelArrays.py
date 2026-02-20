f08fb9b63af7480fab7f885cbe31546950786e59
Goofy-Video-Game <mattosdragon@gmail.com>
2026-02-20 12:58:55 +0000

    Update and rename Parallel arrays.py to ParallelArrays.py

---
+++ ParallelArrays.py
@@ -0,0 +1,7 @@
+names = ["jamal", "jimbob", "henry"]
+ages = [16, 51, 67]
+weight = [60, 70, 90]
+# first value in each array belong to the same person, same with second value, third, fourth
+
+
+print(names[0], ages[0], weight[0]) #should output jamal, 16, 60
