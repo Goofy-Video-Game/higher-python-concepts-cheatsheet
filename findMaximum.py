32ba7b23907b6a60c0020ca6cf9e08d76b2e4a57
Goofy-Video-Game <mattosdragon@gmail.com>
2026-02-23 13:46:23 +0000

    Update findMaximum.py

--- findMaximum.py
+++ findMaximum.py
@@ -1,3 +1,9 @@
 unsortedArrayOfItems = [1, 6, 9, 5, 4, 3, 2, 1, 10, 22, 31, 2, 8] # list to find highest value item within
 
-for j in range(len(unsortedArrayOfItems)): # this loops for the amount of items in the array
+maximum = unsortedArrayOfItems[0] # setup maximum as first value in list for comparing against
+
+for j in range(len(unsortedArrayOfItems)):
+  if unsortedArrayOfItems[0] > maximum:
+    maximum = unsortedArrayOfItems[0] # if value in list larger than current maximum set that to new maximum
+    
+print(unsortedArrayOfItems[0]) # checking it works
